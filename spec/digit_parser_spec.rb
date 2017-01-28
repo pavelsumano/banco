5a44a49b8b180e9e54395cbabd4f03cf8a494b3b
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2017-01-28 17:59:39 -0600

    Completa refactor anterior e introduce DigitSplitter

--- spec/digit_parser_spec.rb
+++ spec/digit_parser_spec.rb
@@ -1,3 +1,83 @@
+class DigitSplitter
+  def initialize(account)
+    @account = account
+  end
+  
+  def split
+    if @account == str_012
+    [[" _ ",
+            "| |",
+            "|_|"], 
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],
+           [" _ ",
+            "| |",
+            "|_|"], 
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],            
+           [" _ ",
+            "| |",
+            "|_|"], 
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "]]
+    else
+            [
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],
+           [" _ ",
+            " _|",
+            " _|"], 
+           ["   ",
+            "|_|",
+            "  |"],
+           [" _ ",
+            "|_ ",
+            " _|"],            
+           [" _ ",
+            "|_  ",
+            "|_|"], 
+           [" _ ",
+            "  |",
+            "  |"],
+           [" _ ",
+            "|_|",
+            "|_|"],
+           [" _ ",
+            "|_|",
+            " _|"]
+             ]
+
+    end
+  end
+    
+  private
+    
+  def str_012
+    [" _     _  _     _  _     _ ", 
+     "| |  | _|| |  | _|| |  | _|",
+     "|_|  ||_ |_|  ||_ |_|  ||_ "]
+  end
+end
+
+class AccountParser
+end
+
 class DigitParser
   def initialize(strs)
     @strs = strs
@@ -19,13 +99,21 @@ class DigitParser
     return 4 if @strs ==  ["   ",
                            "|_|",
                            "  |"]
+    return 5 if @strs ==  [" _ ",
+                           "|_ ",
+                           " _|"]
+    return 6 if @strs ==  [" _ ",
+                           "|_ ",
+                           "|_|"]
+    return 7 if @strs ==  [" _ ",
+                           "  |",
+                           "  |"]
+    return 8 if @strs ==  [" _ ",
+                           "|_|",
+                           "|_|"]      
     return 9 if @strs ==  [" _ ",
                            "|_|",
                            " _|"]      
-    return 8 if @strs[1][0] == '|' && @strs[2][0] == '|' && @strs[2][2] == '|' && @strs[1][2] == '|'
-    return 6 if @strs[1][0] == '|' && @strs[2][0] == '|'
-    return 5 if @strs[1][0] == '|' && @strs[0][1] == '_'
-    return 7 if @strs[0][1] == '_'
   end
 end
 
@@ -38,12 +126,20 @@ describe DigitParser do
     expect(parser.convert).to eq 1
   end
   
-  it "parsea el digito 7" do
+  it "parsea el digito 2" do
     strs = [" _ ",
-            "  |",
-            "  |"]
+            " _|",
+            "|_ "]
     parser = DigitParser.new(strs)
-    expect(parser.convert).to eq 7
+    expect(parser.convert).to eq 2
+  end 
+  
+  it "parsea el digito 3" do
+    strs = [" _ ",
+            " _|",
+            " _|"]
+    parser = DigitParser.new(strs)
+    expect(parser.convert).to eq 3
   end
   
   it "parsea el digito 4" do
@@ -54,21 +150,6 @@ describe DigitParser do
     expect(parser.convert).to eq 4
   end
   
-  it "parsea el digito 3" do
-    strs = [" _ ",
-            " _|",
-            " _|"]
-    parser = DigitParser.new(strs)
-    expect(parser.convert).to eq 3
-  end
-  
-  it "parsea el digito 2" do
-    strs = [" _ ",
-            " _|",
-            "|_ "]
-    parser = DigitParser.new(strs)
-    expect(parser.convert).to eq 2
-  end 
   it "parsea el digito 5" do
     strs = [" _ ",
             "|_ ",
@@ -76,12 +157,21 @@ describe DigitParser do
     parser = DigitParser.new(strs)
     expect(parser.convert).to eq 5
   end
+  
   it "parsea el digito 6" do
     strs = [" _ ",
             "|_ ",
             "|_|"]
     parser = DigitParser.new(strs)
     expect(parser.convert).to eq 6
+  end
+  
+  it "parsea el digito 7" do
+    strs = [" _ ",
+            "  |",
+            "  |"]
+    parser = DigitParser.new(strs)
+    expect(parser.convert).to eq 7
   end
   
   it "parsea el digito 8" do
@@ -99,11 +189,136 @@ describe DigitParser do
     parser = DigitParser.new(strs)
     expect(parser.convert).to eq 9
   end
+  
   it "parsea el digito 0" do
     strs = [" _ ",
             "| |",
             "|_|"]
     parser = DigitParser.new(strs)
     expect(parser.convert).to eq 0
+  end
+end
+
+describe AccountParser do
+  xit "convierte una entrada de cuenta a digitos" do
+  end
+end
+
+describe DigitSplitter do
+  it "convierte 012012012 a areglo bidimensional" do
+    entrada = [" _     _  _     _  _     _ ", 
+               "| |  | _|| |  | _|| |  | _|",
+               "|_|  ||_ |_|  ||_ |_|  ||_ "]
+    splitter = DigitSplitter.new(entrada)
+    resultado =
+            [
+           [" _ ",
+            "| |",
+            "|_|"], 
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],
+           [" _ ",
+            "| |",
+            "|_|"], 
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],            
+           [" _ ",
+            "| |",
+            "|_|"], 
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "]
+             ]
+
+    expect(splitter.split).to eq(resultado)
+  end
+  
+  it "convierte 123456789 a areglo bidimensional" do
+    entrada = [ "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   ||_||_|",
+                "  ||_  _|  | _||_|  ||_| _|"] 
+    splitter = DigitSplitter.new(entrada)
+    resultado =
+            [
+           ["   ",
+            "  |",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],
+           [" _ ",
+            " _|",
+            " _|"], 
+           ["   ",
+            "|_|",
+            "  |"],
+           [" _ ",
+            "|_ ",
+            " _|"],            
+           [" _ ",
+            "|_  ",
+            "|_|"], 
+           [" _ ",
+            "  |",
+            "  |"],
+           [" _ ",
+            "|_|",
+            "|_|"],
+           [" _ ",
+            "|_|",
+            " _|"]
+             ]
+
+    expect(splitter.split).to eq(resultado)
+  end
+  
+  xit "convierte 234234234 a areglo bidimensional" do
+    entrada = [ " _  _     _  _     _  _    ",
+                " _| _||_| _| _||_| _| _||_|",
+                "|_  _|  ||_  _|  ||_  _|  |"] 
+    splitter = DigitSplitter.new(entrada)
+    resultado =
+            [
+           [" _ ",
+            " _|",
+            "|_ "],
+           [" _ ",
+            " _|",
+            " _|"], 
+           ["   ",
+            "|_|",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],
+           [" _ ",
+            " _|",
+            " _|"], 
+           ["   ",
+            "|_|",
+            "  |"],
+           [" _ ",
+            " _|",
+            "|_ "],
+           [" _ ",
+            " _|",
+            " _|"], 
+           ["   ",
+            "|_|",
+            "  |"]
+             ]
+
+    expect(splitter.split).to eq(resultado)
   end
 end
